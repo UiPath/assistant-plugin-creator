50c4845a308d13d1650ade43b9576e178d28dabf
Andrei Vasilescu <andrei.vasilescu@uipath.com>
2020-09-24 17:40:07 +0300

    expose widget icon, title and description

---
+++ projects/sample-widget/src/lib/widget-metadata.ts
@@ -0,0 +1,63 @@
+export const widgetMaterialIcon = 'widgets';
+
+export const getWidgetTitle = lang => {
+  switch (lang) {
+    case 'en':
+      return 'Sample Widget';
+    case 'ja':
+      return 'TODO_TITLE';
+    case 'fr':
+      return 'TODO_TITLE';
+    case 'ru':
+      return 'TODO_TITLE';
+    case 'zh-CN':
+      return 'TODO_TITLE';
+    case 'de':
+      return 'TODO_TITLE';
+    case 'es':
+      return 'TODO_TITLE';
+    case 'es-MX':
+      return 'TODO_TITLE';
+    case 'pt':
+      return 'TODO_TITLE';
+    case 'pt-BR':
+      return 'TODO_TITLE';
+    case 'ko':
+      return 'TODO_TITLE';
+    case 'tr':
+      return 'TODO_TITLE';
+    default:
+      return 'TODO_TITLE';
+  }
+};
+
+export const getWidgetDescription = lang => {
+  switch (lang) {
+    case 'en':
+      return 'Created using github.com/UiPath/assistant-plugin-creator';
+    case 'ja':
+      return 'TODO_DESCRIPTION';
+    case 'fr':
+      return 'TODO_DESCRIPTION';
+    case 'ru':
+      return 'TODO_DESCRIPTION';
+    case 'zh-CN':
+      return 'TODO_DESCRIPTION';
+    case 'de':
+      return 'TODO_DESCRIPTION';
+    case 'es':
+      return 'TODO_DESCRIPTION';
+    case 'es-MX':
+      return 'TODO_DESCRIPTION';
+    case 'pt':
+      return 'TODO_DESCRIPTION';
+    case 'pt-BR':
+      return 'TODO_DESCRIPTION';
+    case 'ko':
+      return 'TODO_DESCRIPTION';
+    case 'tr':
+      return 'TODO_DESCRIPTION';
+    default:
+      return 'TODO_DESCRIPTION';
+  }
+};
