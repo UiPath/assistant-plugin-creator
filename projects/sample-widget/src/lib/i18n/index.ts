b48b30cc0c8f04e79a629f0cd6e45bcf13c15d40
Andrei Vasilescu <andrei.vasilescu@uipath.com>
2021-02-08 16:20:04 +0200

    add i18n support

---
+++ projects/sample-widget/src/lib/i18n/index.ts
@@ -0,0 +1,29 @@
+import * as de from './de.json';
+import * as en from './en.json';
+import * as esMX from './es-MX.json';
+import * as es from './es.json';
+import * as fr from './fr.json';
+import * as ja from './ja.json';
+import * as ko from './ko.json';
+import * as ptBR from './pt-BR.json';
+import * as pt from './pt.json';
+import * as ru from './ru.json';
+import * as tr from './tr.json';
+import * as zhCN from './zh-CN.json';
+
+export const i18n = {
+  en,
+  de,
+  'es-MX': esMX,
+  es,
+  fr,
+  ja,
+  ko,
+  'pt-BR': ptBR,
+  pt,
+  ru,
+  tr,
+  'zh-CN': zhCN,
+};
+
+export * from './translation.pipe';
---
+++ projects/sample-widget/src/lib/i18n/translation.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { TranslationPipe } from './translation.pipe';
+
+describe('TranslationPipe', () => {
+  it('create an instance', () => {
+    const pipe = new TranslationPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
