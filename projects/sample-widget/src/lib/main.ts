4ce06b0fdd23b9eb73792362f3d0d4b8decdcf64
Rares Baesu <rares.baesu@uipath.com>
2025-07-16 14:11:20 +0300

    feat: settings widget

--- projects/sample-widget/src/lib/main.ts
+++ projects/sample-widget/src/lib/main.ts
@@ -308,3 +308,26 @@ export class MainComponent extends HTMLElement {
     return `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="rgb(82, 96, 105)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-download"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="7 10 12 15 17 10"></polyline><line x1="12" y1="15" x2="12" y2="3"></line></svg>`;
   }
 }
+
+
+const settingsTemplate = document.createElement("template");
+settingsTemplate.innerHTML = `
+  <div>
+    <h1>Settings page</h1>
+  </div>
+`
+
+export class SettingsComponent extends HTMLElement {
+  constructor() {
+    super()
+  }
+
+  connectedCallback() {
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(settingsTemplate.content.cloneNode(true));
+    const button = document.createElement('button');
+    button.innerText = "Click me to focus the widget tab!";
+    button.addEventListener('click', () => api.focusWidgetTab());
+    this.shadowRoot.appendChild(button);
+  }
+}
--- projects/sample-widget/src/public-api.ts
+++ projects/sample-widget/src/public-api.ts
@@ -4,3 +4,8 @@ export { i18n } from './lib/i18n';
 export const widgetMaterialIcon = 'widgets';
 export const isTabHidden = false;
 export const usesWebComponent = true;
+
+
+export const settingsWidgetMaterialIcon = 'settings16';
+export const settingsIsBeforeGeneral = true;
+export const settingsIsTabHidden = false;
